fea7b874cfd06face9fb14903561ba679701815d
Brunno dos Santos <squiter85@gmail.com>
2012-10-26 17:01:29 -0200

    Configuration of ActiveRecord

--- config/application.php
+++ config/application.php
@@ -1,8 +1,23 @@
 <?php
+## Slim configurations
+#######################
+require "vendor/autoload.php";
 $app = new \Slim\Slim();
 
 # Configurations for Slim
 $app->setName('Slim Teste');
 $app->config('debug', true);
+
+
+
+## PHP Active Record Configuration
+###################################
+require "vendor/php-activerecord/ActiveRecord.php";
+ActiveRecord\Config::initialize(function($cfg){
+  $cfg->set_model_directory('models');
+  $cfg->set_connections(array(
+    'development' => 'mysql://root@localhost/slim'
+  ));
+});
 
 # End of application.php
